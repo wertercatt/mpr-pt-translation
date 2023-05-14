124977f1ea7e43eacf5c0731ae6cefa348009f22
Blaze Alan Marshall <wertercatt@wertercatt.com>
2023-05-14 16:36:21 -0500

    Add ash compression step

--- tools/5.app-build.bat
+++ tools/5.app-build.bat
@@ -1,9 +1,25 @@
 cd 00000005
 for /d %%i in (.\*) do (
     cd %%~nxi
+    del l10n_ui.arc
+    del man.brres
+    del msg.arc
+    del esrborn.arc
+    del l10n_ui.arc.ash
+    del man.brres.ash
+    del msg.arc.ash
+    del esrborn.arc.ash
     wszst CREATE l10n_ui -a -v -d l10n_ui.arc
+    ashcompress l10n_ui.arc
+    del l10n_ui.arc
     wszst CREATE man -a -v
+    ashcompress man.brres
+    del man.brres
     wszst CREATE msg -a -v -d msg.arc
+    ashcompress msg.arc
+    del msg.arc
     wszst CREATE esrborn -a -v -d esrborn.arc
+    ashcompress esrborn.arc
+    del esrborn.arc
     cd ..
 )
